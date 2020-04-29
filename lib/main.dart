0967fe04321de4c42a917ed0e015b805725fc6d2
Luiz Casula <luizcasulaf@gmail.com>
2020-04-29 00:12:32 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:busy_time/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
