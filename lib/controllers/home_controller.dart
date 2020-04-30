cdbaaf233b35c153e9dbc0c2ffb7d8fbb7c48356
Luiz Casula <luizcasulaf@gmail.com>
2020-04-30 00:58:46 -0400

    reorganized class

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 
 import 'package:busy_time/models/content_model.dart';
 import 'package:busy_time/repositories/content_repository.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_controller.g.dart';
@@ -9,6 +10,9 @@ class HomeController = _HomeController with _$HomeController;
 
 abstract class _HomeController with Store {
   
+  
+  ContentRepository _repository;
+
   _HomeController(){
     _repository = ContentRepository();
   }
@@ -17,17 +21,16 @@ abstract class _HomeController with Store {
   @observable
   bool status = true;
 
+  
   List<ContentModel> listContent = List();
 
-  ContentRepository _repository;
-  
-    
-  
+
   @action
   getContent(String query) async {
     status = false;
     ContentModel model = await _repository.getContent(query);
     listContent.add(model);
+    //Future.delayed(new Duration(milliseconds: 1500));
     status = true;
   }
 
