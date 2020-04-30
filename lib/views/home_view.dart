64fd7622ba812fd88b93d043d3c6dcf741ea762c
Luiz Casula <luizcasulaf@gmail.com>
2020-04-29 23:20:28 -0400

    result from api

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:busy_time/components/custom_search_delagate.dart';
 import 'package:busy_time/controllers/home_controller.dart';
 import 'package:busy_time/models/content_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key key}) : super(key: key);
@@ -27,13 +28,7 @@ class _HomeViewState extends State<HomeView> {
             delegate: CustomSearchDelagate(hintText: 'Series or Movies'),
           );
           if (query != null) {
-            setState((){
-              _controller.status = false;
-              _controller.getContent(query);
-              _controller.status = true;
-            });
-            
-            
+            await _controller.getContent(query);
           }
         },
       ),
@@ -41,24 +36,28 @@ class _HomeViewState extends State<HomeView> {
         child: Container(
           padding: EdgeInsets.all(16),
           height: double.maxFinite,
-          child: _controller.status
-              ? _controller.listContent == null
-                  ? Center(
-                      child: Text("empty"),
-                    )
-                  : ListView.builder(
-                      itemCount: _controller.listContent.length,
-                      itemBuilder: (context, index) {
-                        print("item builder index: " + index.toString());
-                        ContentModel model = _controller.listContent[index];
-                        return ListTile(
-                          title: Text(model.name),
-                        );
-                      },
-                    )
-              : Center(
-                  child: CircularProgressIndicator(),
-                ),
+          child: Observer(
+            builder: (context) {
+              return _controller.status
+                  ? _controller.listContent == null
+                      ? Center(
+                          child: Text("empty"),
+                        )
+                      : ListView.builder(
+                          itemCount: _controller.listContent.length,
+                          itemBuilder: (context, index) {
+                            print("item builder index: " + index.toString());
+                            ContentModel model = _controller.listContent[index];
+                            return ListTile(
+                              title: Text(model.name),
+                            );
+                          },
+                        )
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    );
+            },
+          ),
         ),
       ),
     );
